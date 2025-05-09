751c8b8116782bc9ab45a3df22ed48929540c80c
SaminSadeghipour <sadeghipoursamin@gmail.com>
2025-05-09 22:21:15 +0330

    bruh

--- core/src/main/java/com/example/Views/SignupMenuView.java
+++ core/src/main/java/com/example/Views/SignupMenuView.java
@@ -22,6 +22,10 @@ public class SignupMenuView implements Screen {
     private final TextField securityQuestion;
     private final Label securityQuestionLabel;
     private final TextButton guestButton;
+    private String enteredUsername;
+    private String enteredPassword;
+    private String enteredSecurityQuestion;
+
 
     public SignupMenuView(SignupMenuController controller, Skin skin) {
         this.controller = controller;
@@ -34,6 +38,9 @@ public class SignupMenuView implements Screen {
         this.guestButton = new TextButton("Guest", skin);
         this.signupTitle = new Label("Signup Menu", skin);
         this.table = new Table();
+        this.enteredPassword = null;
+        this.enteredUsername = null;
+        this.enteredSecurityQuestion = null;
         controller.setView(this);
     }
 
@@ -43,17 +50,27 @@ public class SignupMenuView implements Screen {
         Gdx.input.setInputProcessor(stage);
 
         table.setFillParent(true);
-        table.center();
-        table.add(signupTitle);
-        table.row().pad(20, 400, 20, 0);
-        table.add(username).width(400);
-        table.row().pad(20, 400, 20, 0);
-        table.add(password).width(400);
-        table.row().pad(20, 400, 20, 0);
-        table.add(securityQuestion).width(400);
-        table.row().pad(20, 400, 20, 0);
-        table.add(guestButton);
+        table.add(signupTitle).colspan(2).center();
+        table.row().pad(20, 0, 20, 0);
+
+        //username
+        table.add(usernameLabel).height(40).padRight(20);
+        table.add(username).width(400).height(80);
+        table.row().pad(20, 0, 20, 0);
+
+        //password
+        table.add(passwordLabel).height(40).padRight(20);
+        table.add(password).width(400).height(80);
+        table.row().pad(20, 0, 20, 0);
+
+        // Security question
+        table.add(securityQuestionLabel).height(40).padRight(20);
+        table.add(securityQuestion).width(400).height(80);
+        table.row().pad(20, 0, 20, 0);
+
+        table.add(guestButton).colspan(2).center();
         table.row().pad(10, 0, 10, 0);
+
 
         stage.addActor(table);
     }
